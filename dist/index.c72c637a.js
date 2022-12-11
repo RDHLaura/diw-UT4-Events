04084852638c210be3f25c32dbda9559ce1834a2
rdhlaura <laurarodriguezdeharo@outlook.es>
2022-12-11 10:06:38 +0100

    añadidas todas las paginas a falta de la 404

---
+++ dist/index.c72c637a.js
@@ -0,0 +1,10 @@
+function fav(id) {
+    const myfav = document.getElementById(id);
+    myfav.classList.toggle("activo");
+}
+function desplegar_menu() {
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("activo");
+}
+
+//# sourceMappingURL=index.c72c637a.js.map
---
+++ src/assets/js/scripts.js
@@ -0,0 +1,11 @@
+function fav(id){
+    const myfav = document.getElementById(id);
+    myfav.classList.toggle('activo')
+    
+}
+
+
+function desplegar_menu(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('activo')
+}
