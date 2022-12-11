14f52569adee3e33512a711128af97146e84d58f
rdhlaura <laurarodriguezdeharo@outlook.es>
2022-12-11 23:38:15 +0100

    audio a la pag 404

--- dist/index.862c4df5.js
+++
@@ -1,2 +0,0 @@
-function fav(o){document.getElementById(o).classList.toggle("fa-heart-active")}function desplegar_menu(){document.getElementById("nav").classList.toggle("activo")}let modoclaroOn=!1;function claro_oscuro(){if(document.body.classList.toggle("theme--light"),document.body.classList.contains("theme--light")){}}function checkMode(){modoclaroOn&&document.body.classList.add("theme--light"),console.log(modoclaroOn)}
-//# sourceMappingURL=index.862c4df5.js.map
