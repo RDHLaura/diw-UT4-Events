ec0d6f57d0edb15864a85c6fba182a528a61bc7f
rdhlaura <laurarodriguezdeharo@outlook.es>
2022-12-11 22:49:26 +0100

    run build ejecutado

---
+++ dist/index.862c4df5.js
@@ -0,0 +1,2 @@
+function fav(o){document.getElementById(o).classList.toggle("fa-heart-active")}function desplegar_menu(){document.getElementById("nav").classList.toggle("activo")}let modoclaroOn=!1;function claro_oscuro(){if(document.body.classList.toggle("theme--light"),document.body.classList.contains("theme--light")){}}function checkMode(){modoclaroOn&&document.body.classList.add("theme--light"),console.log(modoclaroOn)}
+//# sourceMappingURL=index.862c4df5.js.map
